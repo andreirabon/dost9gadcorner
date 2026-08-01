aef7f5c28ede6cd24328d8573107600b48ca6f93
andreirabon <andreirabon@gmail.com>
2026-08-01 08:49:07 +0800

    Update

---
+++ public/build/assets/ReportBackArrowIcon.vue_vue_type_script_setup_true_lang-DBczeev3.js
@@ -0,0 +1,1 @@
+import{A as r}from"./arrow-left-jT92wRYp.js";import{d as e,o,h as s,u as t}from"./app-DacT9Ukd.js";const _=e({__name:"ReportBackArrowIcon",setup(a){return(n,c)=>(o(),s(t(r),{class:"size-4 shrink-0","stroke-width":2,"aria-hidden":"true"}))}});export{_};
